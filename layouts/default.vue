c9d6b6f9bc613fd56d105531f7dfec59d49913bf
hivesbee <hivesbee@gmail.com>
2018-04-15 03:04:14 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container is-fullhd">
+    <nav class="level">
+      <div class="level-left">
+        <h1>inugifs</h1>
+      </div>
+      <div class="level-right">
+        <p class="level-item">upload</p>
+        <p class="level-item">show</p>
+        <p class="level-item">random</p>
+      </div>
+    </nav>
+    <div class="notification">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "default"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+ /*
+  ** Include css not in components
+  */
+  css: [
+    // node.js module but we specify the pre-processor
+    { src: '~assets/main.scss', lang: 'scss' }
+  ]
+}
